bd798eb6ff64ec59f29b9d5576650787d1d0c995
Manicpressure <minwookb@gmail.com>
2023-09-23 22:44:38 +1200

    Pleas Deleting the unmoved files :the commit message for your changes. Lines starting

--- qsort.py
+++
@@ -1,8 +0,0 @@
-def qsort(L):
-    if L:
-        return qsort([x for x in L[1:] if x < L[0]]) \
-        + L[:1] + qsort([x for x in L[1:] if x >= L[0]])
-    return []
-
-list = [0, 33, 22, 11, 55, 44]
-print(qsort(list))
